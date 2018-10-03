099db1d17a036fa2c295116459e909adda6d8c98
Arjav Jain <31507107+arjavjain59998@users.noreply.github.com>
2018-10-03 22:33:00 +0530

    change for pull request

--- first/views.py
+++ first/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    var = {'insert_me':"im from veiws.py"}
+    var = {'insert_me':"im from veiws.py and pull request"}
     return render(request,'index.html',context = var)
 
 
