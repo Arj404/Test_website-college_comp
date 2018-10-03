a8bf6388ed808f1f10f6b01ba3700ab1001163ea
Arjav Jain <31507107+arjavjain59998@users.noreply.github.com>
2018-10-03 22:34:46 +0530

    Update views.py

--- first/views.py
+++ first/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    var = {'insert_me':"im from veiws.py and pull request"}
+    var = {'insert_me':"im from veiws.py and pull request 2"}
     return render(request,'index.html',context = var)
 
 
